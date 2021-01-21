5bac6461c55a634da6ce99a79df371eaacdb5388
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-01-21 16:03:24 -0500

    show Ben

---
+++ Makefile
@@ -0,0 +1,39 @@
+## This is dictClean
+
+current: target
+-include target.mk
+
+# -include makestuff/perl.def
+
+vim_session:
+	bash -cl "vmt"
+
+######################################################################
+
+## pkgall:
+## quickinstall:
+
+######################################################################
+
+
+######################################################################
+
+### Makestuff
+
+Sources += Makefile
+
+Ignore += makestuff
+msrepo = https://github.com/dushoff
+
+Makefile: makestuff/Makefile
+makestuff/Makefile:
+	git clone $(msrepo)/makestuff
+	ls makestuff/Makefile
+
+-include makestuff/os.mk
+
+-include makestuff/pipeR.mk
+-include makestuff/rpkg.mk
+
+-include makestuff/git.mk
+-include makestuff/visual.mk
---
+++ R/functions.R
@@ -0,0 +1,73 @@
+#' @importFrom tibble tibble
+#' @importFrom dplyr left_join filter distinct
+#' @importFrom dplyr mutate pull select %>%
+NULL
+
+#' merge a data vector with a dictionary table
+#' @export
+#' @param dat data vector
+#' @param dict dictionary table
+#' @param keys dictionary keys
+#' @param values dictionary values
+mergeDict <- function(dat, dict=NULL, keys, values){
+	if (is.null(dict)) dict = cleanDict(keys, values)
+	return(left_join(
+		tibble(x=dat), dict
+	))
+}
+
+#' Make a dictionary tibble from a keys vector and a values vector
+#' @function cleanDict
+#' @export
+#' @param keys dictionary keys
+#' @param values dictionary values
+cleanDict <- function(keys, values){
+	if (
+		!is.vector(keys) | !is.vector(values)
+		| length(keys) != length(values)
+	) stop("Need two equal-length vectors for cleanDict")
+	r <- (tibble(x=keys, n=values))
+	o <- filter(r, !is.na(x) & !is.na(n))
+	if (nrow(o) < nrow(r))
+		warning("Omitting NAs in cleanDict")
+	d <- distinct(o)
+	if (nrow(d) < nrow(o))
+		warning("Omitting repeated entries in cleanDict")
+	c <- d %>% select(x) %>% distinct()
+	if (nrow(c) < nrow(d))
+		stop("Ambiguous entries found in cleanDict")
+	return(d)
+}
+
+#' use mergeDict to patch errors identified in a table
+#' @export
+#' @param dat data vector
+#' @param dict dictionary table
+#' @param keys dictionary keys
+#' @param values dictionary values
+patchDict <- function(dat, dict=NULL, keys, values){
+	return(mergeDict(dat, dict, keys, values)
+		%>% mutate(n=ifelse(is.na(n), x, n))
+		%>% pull(n)
+	)
+}
+
+#' use mergeDict to apply a lookup table
+#' @export
+#' @param dat data vector
+#' @param dict dictionary table
+#' @param keys dictionary keys
+#' @param values dictionary values
+catDict <- function(dat, dict=NULL, keys, values){
+	m <- (mergeDict(dat, keys, values))
+	not_found <- filter(m, 
+		!is.na(x) & is.na(n)
+	)
+	if (nrow(not_found) > 0){
+		cat("Items not found in catDict")
+		warning("Items not found in catDict"
+			, unique(not_found[["x"]])
+		)
+	}
+	return(pull(m, n))
+}
