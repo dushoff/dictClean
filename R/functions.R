35a55ca19317bab73942e9bbb5010f371414a976
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-05-10 23:54:15 -0400

    good night

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,5 @@
+## FIXME: attach an attribute to dict objects; don't clean them (instant return)
+
 #' @importFrom tibble tibble
 #' @importFrom dplyr left_join filter distinct
 #' @importFrom dplyr mutate pull select %>%
@@ -6,11 +8,11 @@ NULL
 #' merge a data vector with a dictionary table
 #' @export
 #' @param dat data vector
-#' @param dict dictionary table
+#' @param df dictionary table
 #' @param keys dictionary keys
 #' @param values dictionary values
-mergeDict <- function(dat, dict=NULL, keys, values){
-	if (is.null(dict)) dict = cleanDict(keys, values)
+mergeDict <- function(dat, df=NULL, keys, values){
+	dict = cleanDict(df, keys, values)
 	return(left_join(
 		tibble(x=dat), dict
 	))
@@ -21,7 +23,11 @@ mergeDict <- function(dat, dict=NULL, keys, values){
 #' @export
 #' @param keys dictionary keys
 #' @param values dictionary values
-cleanDict <- function(keys, values){
+cleanDict <- function(df=NULL, keys, values){
+	if (!is.null(df)){
+		keys = df[[1]]
+		values = df[[2]]
+	}
 	if (
 		!is.vector(keys) | !is.vector(values)
 		| length(keys) != length(values)
@@ -42,11 +48,11 @@ cleanDict <- function(keys, values){
 #' use mergeDict to patch errors identified in a table
 #' @export
 #' @param dat data vector
-#' @param dict dictionary table
+#' @param df dictionary table
 #' @param keys dictionary keys
 #' @param values dictionary values
-patchDict <- function(dat, dict=NULL, keys, values){
-	return(mergeDict(dat, dict, keys, values)
+patchDict <- function(dat, df=NULL, keys, values){
+	return(mergeDict(dat, df, keys, values)
 		%>% mutate(n=ifelse(is.na(n), x, n))
 		%>% pull("n")
 	)
@@ -55,18 +61,18 @@ patchDict <- function(dat, dict=NULL, keys, values){
 #' use mergeDict to apply a lookup table
 #' @export
 #' @param dat data vector
-#' @param dict dictionary table
+#' @param df dictionary table
 #' @param keys dictionary keys
 #' @param values dictionary values
-catDict <- function(dat, dict=NULL, keys, values){
-	m <- (mergeDict(dat, keys, values))
+catDict <- function(dat, df=NULL, keys, values){
+	m <- mergeDict(dat, df, keys, values)
 	not_found <- dplyr::filter(m, 
 		!is.na(x) & is.na(n)
 	)
 	if (nrow(not_found) > 0){
 		cat("Items not found in catDict")
-		warning("Items not found in catDict"
-			, unique(not_found[["x"]])
+		warning("Items not found in catDict: "
+			, paste(unique(not_found[["x"]]), collapse=" | ")
 		)
 	}
 	return(pull(m, "n"))
